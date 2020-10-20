ca6e37ad61fb6f39e7dd476d706fb6ca7fd5df67
Yun Feng <yun.feng@smartx.com>
2020-10-20 13:43:23 +0800

    initiate the project

---
+++ src/Calculator.svelte
@@ -0,0 +1,39 @@
+<script>
+</script>
+
+<style>
+    .Calculator {
+        display: grid;
+        grid-template-columns: repeat(4, 5em);
+        grid-template-rows: repeat(5, 3em);
+        grid-gap: 0.5em;
+    }
+
+    button {
+        margin: 0;
+    }
+</style>
+
+<h1 style="color: #333">result</h1>
+<div class="Calculator">
+    <button>c</button>
+    <button>&lt;</button>
+    <button>%</button>
+    <button>÷</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>x</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>-</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>+</button>
+    <button>clear</button>
+    <button>0</button>
+    <button>.</button>
+    <button>=</button>
+</div>
